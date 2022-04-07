1e751a976294e2b261823d533272f0c91a6f5531
mathocihara <matheusyukio.hocihara@gmail.com>
2022-04-07 19:25:23 -0300

    reformulação da estrutura codigo fonte

--- cifraDeCesar/cifraDeCesar.js
+++ cifraDeCesar/cifraDeCesar.js
@@ -1,49 +1,64 @@
-//select
-var selecao = document.getElementById('codigo'); // aqui está o select para
-var valor = selecao.options[selecao.selectedIndex].value;
+// Declaraçao das variaveis que receberao os elementos do html
+var selectCripto = document.getElementById('selectCripto'); // criei uma variavel que recebe o elemento cripto
+var txtCarta = document.getElementById('txtCarta'); // criei uma variavel para receber o elemento textarea 'txtCarta '
+var rdAcao = document.getElementsByName('rdAcao'); // criei uma radio button que vai receber o elemento 'btncodificacao'
+var txtFinal = document.getElementById('txtFinal'); // criei uma variavel que vai receber o elemento textarea'txtFinal'
+var nmbChave = document.getElementById('nmbChave'); // criei uma variavel que vai receber o elemento number 'nmbChave'
 
-function dinamicaDoSelect(){
-    var selecao = document.getElementById('codigo'); // aqui está o select para
-    var opcoes = selecao.options[selecao.selectedIndex].value;
+// declaração das variaveis que receberão os valores selecionados
+var criptoSelecionada = '';
+var textoCartaEscrito = '';
+var AcaoSelecionada = '';
+var textoFinal = '';
+var ChaveSelecionada = '';
 
-  console.log(opcoes);
+
+//essa funcao ira captar a opção de escolha de criptografi do usuario e mostrar no console log 
+function CriptoEscolhida() {
+  //instanciação de variavel
+  criptoSelecionada = selectCripto.options[selectCripto.selectedIndex].value;
+
+  console.log(criptoSelecionada);
 }
-  //text
-var textoCriptrografar = document.getElementById('text'); // texto que vai ser criptografado
-//botões
-var codificar = document.getElementById('codificar'); // selecao que codifica o texto
-var decodificar = document.getElementById('decodificar'); // selecao que descodifica o texto
-//saida do text
-var textoDescriptografado = document.getElementById('resultado'); // area do texto criptografado 
-//numeros para a cifra de cesar
-var shift = document.getElementById('numeral'); // numeros que serão usado para o cifra de cesar
-// botao de enviar
+
+
+//cxtexto = vou pegar o texto que o usuario me der e fazer uma verificaçao das palavras para poder substituir 
+//na caixa de texto vamos ter uma frase feita pelo usuario, para ser criptografada
+
+
+
+function textoCriptografico() {
+  var textParaCripto = txtCarta.value;
+  console.log(textParaCripto)
+}
+
+
+
+//escolhaDoBtn
+var btnDefinido = '';
+
+function decisaoDoBotao() {
+  btnDefinido = escolha.value;
+  console.log(btnDefinido);
+}
+
+
+//cxResultado
 
 //matrix para a criptografia
-var Alfabeto = [
-    'A', 'B', 'C', 'D', 'E', 'F',
-    'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R',
-    'S', 'T', 'U', 'V', 'W', 'X',
-    'Y', 'Z'
-];
 
+function textoCriptografico() {
+  var textParaCripto = textoPosto.value;
+  console.log(textParaCripto)
+}
 
+//numDaSubsti
+var numDeSubsti = '';
 
+function teste() {
+  numeroDeSubsti = numero.value;
+  console.log(numDeSubsti);
 
+}
 
-
-
-
-
-
-
-
-
-
-//   // enquanto meu codigo estiver sinalizando o cesar, ele  vai pegar as informações por 'get' do textarea e 
-//     // // vai armazenar no setter.
-//     // Após isso vou começar a fazer uma function para poder começar o codigo do cifra de cesar
-//     // vou criar uma variavel matriz do alfabeto
-//     // vou fazer o calculo da cifra de cesar
-//     // vou pedir para ele me retornar o valor em forma de string
+//botao
