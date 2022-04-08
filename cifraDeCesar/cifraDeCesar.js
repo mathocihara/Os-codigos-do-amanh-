39e63208914212d02c7c6f5e87df289e507bdb30
mathocihara <matheusyukio.hocihara@gmail.com>
2022-04-07 23:11:27 -0300

    Adicionei os codigos

--- cifraDeCesar/cifraDeCesar.js
+++ cifraDeCesar/cifraDeCesar.js
@@ -9,22 +9,24 @@ var nmbChave = document.getElementById('nmbChave'); // criei uma variavel que va
 var criptoSelecionada = '';
 var textoCartaEscrito = '';
 var acaoSelecionada = '';
-var chaveSelecionada = '';
+var chaveSelecionada = 0;
 
 // declaração do alfabeto para a Cifra de Cesar
 var alfabeto = [
-  "a","b","c","d","e","f","g","h"
-  ,"i","j","k","l","m","n","o","p"
-  ,"q","r","s","t","u","v","w","x"
-  ,"y","z"
+  'a','b','c','d','e','f','g','h'
+  ,'i','j','k','l','m','n','o','p'
+  ,'q','r','s','t','u','v','w','x'
+  ,'y','z'
 ];
+//
+var novosChar = [];
 
 // a funcao vai receber os elementos html e armazenar seus respectivos valores para preparar a criptografia ou descriptografia do texto finl
 function PrepararCarta() {
   // utilizamos as variaveis dos elementos para obter seus valores
   criptoSelecionada = selectCripto.options[selectCripto.selectedIndex].value;
-  textoCartaEscrito = txtCarta.value;
-  chaveSelecionada = nmbChave.value;
+  textoCartaEscrito = txtCarta.value.toLowerCase();
+  chaveSelecionada = nmbChave.value; 
 
   for( i = 0; i < rdAcao.length; i++){
     if(rdAcao[i].checked){
@@ -42,9 +44,52 @@ function PrepararCarta() {
   }
   
   if(criptoSelecionada == "cifraCesar"){
-                  
-  } 
+    if( acaoSelecionada == 'cripto'){
+      console.log('criptografando...');
+      for(i= 0; i < textoCartaEscrito.length; i++){
+        if( textoCartaEscrito[i] != ' '){
+          for(j = 0; j < alfabeto.length; j++){
+            if(textoCartaEscrito[i] == alfabeto[j] ){
+              novosChar[i] = alfabeto[(j+chaveSelecionada)%alfabeto.length]
+            }
+          }
+        }
+        else {
+          novosChar[i] = ' ';
+        }
+      }
+    }
+    else{
+      console.log('descriptografando...');
+      for(i= 0; i < textoCartaEscrito.length; i++){
+        if( textoCartaEscrito[i] != ' '){
+          for(j = 0; j < alfabeto.length; j++){
+            if(textoCartaEscrito[i] == alfabeto[j] ){
+              novosChar[i] = alfabeto[(j-chaveSelecionada)];
+            }
+          }
+        }
+        else {
+          novosChar[i] = ' ';
+        }
 
+      }   
+    }
+
+
+    txtFinal.value = novosChar.join("");
+    
+  } else {
+    if( acaoSelecionada == 'cripto'){
+      txtFinal.value = btoa(textoCartaEscrito);
+      
+    }
+    else{
+      txtFinal.value = atob(textoCartaEscrito);
+      
+    }
+
+  }
 
 
 
